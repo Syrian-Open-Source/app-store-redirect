d3719800e9e1c04a859ac3a266254ba86ec49189
Somar <kesen.somar.99@gmail.com>
2024-01-29 11:55:58 +0300

    Do not use PHP Short Tags #9

--- shortcode-app-redirect-button.php
+++ shortcode-app-redirect-button.php
@@ -1,7 +1,7 @@
 <?php if (!defined('ABSPATH')) exit; // Exit if accessed directly 
 ?>
 <!-- App Store button -->
-<a href="<?= site_url() . get_option($data['app_link_setting']) ?>" target="_blank" class="as-btn <?= $data['button_class'] ?>" role="button">
-    <span class="as-button-subtitle"><?= $data['button_sub_title'] ?></span>
-    <span class="as-button-title"><?= $data['button_title'] ?></span>
+<a href="<?php echo site_url() . get_option($data['app_link_setting']); ?>" target="_blank" class="as-btn <?php echo $data['button_class']; ?>" role="button">
+    <span class="as-button-subtitle"><?php echo $data['button_sub_title']; ?></span>
+    <span class="as-button-title"><?php echo $data['button_title']; ?></span>
 </a>
